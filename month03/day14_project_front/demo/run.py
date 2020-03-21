b96292198f2664e6f0d03704be67d2d99865181e
hhh <1085414029@qq.com>
2020-03-21 18:07:53 +0800

    2020/3/21 bootstrap

--- month03/day14_project_front/demo/run.py
+++ month03/day14_project_front/demo/run.py
@@ -6,7 +6,9 @@ webapp = Flask(__name__)
 @webapp.route('/demo')
 def demo_view():
     return render_template('demo.html')
-
+@webapp.route('/index')
+def index_view():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     webapp.run(debug=True)
